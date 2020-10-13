23e4ca435b1db96cf769cc4e71bc93bd1bc0be15
文飞 <353186996@qq.com>
2020-10-13 11:08:13 +0800

    v1013

--- app/router/default.js
+++ app/router/default.js
@@ -2,7 +2,7 @@
  * @Author: mikey.wf
  * @Date: 2020-10-09 10:18:53
  * @Last Modified by: mikey.wf
- * @Last Modified time: 2020-10-10 10:37:24
+ * @Last Modified time: 2020-10-12 14:44:49
  */
 'use strict';
 
@@ -11,4 +11,6 @@ module.exports = app => {
   router.get('/default/index', controller.default.home.index);
   router.get('/default/getArticleList', controller.default.home.getArticleList);
   router.get('/default/getArticleById/:id', controller.default.home.getArticleById);
+  router.get('/default/getTypeInfo', controller.default.home.getTypeInfo);
+  router.get('/default/getListById/:id', controller.default.home.getListById);
 };
